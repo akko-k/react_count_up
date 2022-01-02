8e80a35957247cd33adf8463e74713c07049e349
kawakami <barcarolle.op60.horowitz@gmail.com>
2022-01-02 23:49:47 +0900

    タスクの追加機能を実装

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import "./App.css";
 
 const App = () => {
+  const [todoText, settodoText] = useState("");
   const [incompleteTodos, setincompleteTodos] = useState([
     "洗濯物を干す",
     "お皿を洗う",
@@ -10,11 +11,26 @@ const App = () => {
     "ピアノを弾く",
     "歯を磨く",
   ]);
+  const onChangeTodoText = (event) => {
+    settodoText(event.target.value);
+  };
+
+  const onClickAdd = () => {
+    if (todoText === "") return;
+    const newTodos = [...incompleteTodos, todoText];
+    setincompleteTodos(newTodos);
+    settodoText("");
+  };
+
   return (
     <>
       <div className="input-area">
-        <input placeholder="TODOを入力" />
-        <button>追加</button>
+        <input
+          placeholder="TODOを入力"
+          value={todoText}
+          onChange={onChangeTodoText}
+        />
+        <button onClick={onClickAdd}>追加</button>
       </div>
       <div className="incomplete-area">
         <p className="title">未完了のTODO</p>
